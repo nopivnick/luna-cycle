a66ee6539a7a0d54cb044b4051a03d1972bc655d
nopivnick <nopivnick@nopivnick.com>
2020-12-31 14:24:44 -0500

    Update sketch.js
    - toggle mirrorer text ON|OFF works

--- p5/__mirror/sketch.js
+++ p5/__mirror/sketch.js
@@ -1,7 +1,29 @@
+let screen;
+let isMirrored = false;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  screen = select('body');
+}
+
+function mirrorScreen() {
+  screen.style('transform: rotateY(180deg)');
 }
 
 function draw() {
-  background(220);
+  textAlign(CENTER);
+  text("Mirror", windowWidth / 2, windowHeight / 2)
+}
+
+function keyPressed() {
+  if (keyCode === 77) {
+    if (isMirrored == true) {
+      screen.style('transform: none');
+      isMirrored = !isMirrored;
+    } else {
+      mirrorScreen();
+      isMirrored = !isMirrored;
+    }
+    print("is text mirrored? " + isMirrored);
+  }
 }
