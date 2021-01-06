db8ea5f31c9c2adf3a89aa596cd678ff9cd6a171
nopivnick <nopivnick@nopivnick.com>
2021-01-06 17:48:08 -0500

    Add scenes code to main sketch
    - move /_experiment/scenes → /_archive/scenes
    - update /luna-cycle/index.html

--- p5/luna-cycle/sketch.js
+++ p5/luna-cycle/sketch.js
@@ -1,7 +1,34 @@
+let sceneManager;
+let sceneIndex;
+let sceneCount;
+
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
+  sceneManager = new SceneManager();
+  sceneManager.wire();
+
+  // Preload scenes. Preloading is normally optional
+  // ... but needed if showNextScene() is used.
+  // The order in which scenes are added (preloaded) 
+  // determines the scene's index in the scenes array.
+  sceneManager.addScene(scene0);
+  sceneManager.addScene(scene1);
+  sceneManager.addScene(scene2);
+  sceneManager.addScene(scene3);
+  sceneManager.addScene(scene4);
+  // TODO: preload scenes with a for loop
+  // for (let i = 0; i < sceneCount; i++) {
+  //   sceneManager.addScene("scene" + i);
+  //   print("scene" + i + " preloaded!");
+  // }
+
+  sceneManager.showScene(scene0);
+}
+
+function draw() {
+
 }
 
 function windowResized() {
@@ -9,6 +36,22 @@ function windowResized() {
   print("window size: " + windowWidth + " x " + windowHeight);
 }
 
-function draw() {
+function playtestKeyPressed() {
+  print("We're in the playtestKeyPressed() function!");
+  if (keyCode === RIGHT_ARROW) {
+    sceneManager.showNextScene();
+  } else if (keyCode === LEFT_ARROW) {
+    sceneManager.showPreviousScene();
+  }
+  print(key + " pressed!");
+}
 
+function onEnterScene() {
+  clear();
+  // background(175);
+  paragraph = createP("This is " + sceneManager.scene.fnScene.name + ".");
+}
+
+function onExitScene() {
+  removeElements();
 }
