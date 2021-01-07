ec0f5531932abd5a856d2655f292c460bc9cca2e
nopivnick <nopivnick@nopivnick.com>
2021-01-07 12:54:22 -0500

    Refactor with p5 style code

--- p5/_experiments/trackpad/trackpad.js
+++ p5/_experiments/trackpad/trackpad.js
@@ -4,15 +4,22 @@
  * - https://github.com/X123M3-256/X123M3-256.github.io/blob/master/rotationtest.html
  */
 
+// Uncomment below for vanilla javascript code
 let mouseX = 0;
 let mouseY = 0;
 
-trackpadSamples = [
+let trackpadSamples = [
   [0, 0],
   [0, 0],
   [0, 0]
 ];
 
+// Uncomment below for p5 code
+// function setup() {
+//   setInterval("updateSamples()", 100);
+// }
+
+// Uncomment below for vanilla javascript code
 function handleMouseEvent(event) {
   mouseX = event.clientX;
   mouseY = event.clientY;
@@ -31,19 +38,34 @@ function updateSamples() {
   let cross = (x1 * y2) - (y1 * x2);
   if (cross > 0) {
     isSpinningFwd = true;
-    // document.body.innerHTML = "Forward";
+    document.body.innerHTML = "Forward";
+    // Uncomment below for vanilla javascript code
     console.log("isSpinningFwd is " + isSpinningFwd);
+    // Uncomment below for p5 code
+    // print("isSpinningFwd is " + isSpinningFwd);
   } else if (cross < 0) {
     isSpinningBkwd = true;
-    // document.body.innerHTML = "Backward";
+    document.body.innerHTML = "Backward";
+    // Uncomment below for vanilla javascript code
     console.log("isSpinningBkwd is " + isSpinningBkwd);
+    // Uncomment below for p5 code
+    // print("isSpinningBkwd is " + isSpinningBkwd);
   } else {
     isSpinning = false;
-    // document.body.innerHTML = "Stopped";
+    document.body.innerHTML = "Stopped";
+    // Uncomment below for vanilla javascript code
     console.log("isSpinning is " + isSpinning);
+    // Uncomment below for p5 code
+    // print("isSpinning is " + isSpinning);
   }
 }
 
+// Uncomment below for p5 code
+// function mouseMoved() {
+//   updateSamples();
+// }
+
+// Uncomment below for vanilla javascript code
 function init() {
   document.addEventListener("mousemove", handleMouseEvent);
   setInterval("updateSamples()", 100);
