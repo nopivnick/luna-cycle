1f151dead37bb11ebde1fdfde263bbf572d0ed7f
nopivnick <nopivnick@nopivnick.com>
2021-01-20 15:45:23 -0500

    Update sketch.js

--- p5/_experiments/paddle/sketch.js
+++ p5/_experiments/paddle/sketch.js
@@ -1,3 +1,5 @@
+let pot; // values come from the potentiometer via Arduino over serial, the actual value is read off the Steenbeck speed switch, in this example the assumption is it's a value between 0 and 1023
+
 function setup() {
   createCanvas(400, 400);
 }
