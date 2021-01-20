14ad7cb7d76ca5c2c3541f0476ffe63e1e3c49e1
nopivnick <nopivnick@nopivnick.com>
2021-01-20 15:49:05 -0500

    Update sketch.js
    - Remove pot variable
    - Refactor variable & function names

--- p5/_experiments/plate/sketch.js
+++ p5/_experiments/plate/sketch.js
@@ -1,9 +1,8 @@
-let ticks = 0; // ticks come from the rotary encoder via Arduino over serial
-let pot = 0; // values come from the potentiometer via Arduino over serial, the actual value is read off the Steenbeck speed switch, in this example the assumption is it's a value between 0 and 1023
-let stateCurrentIndex = 0;
-let stateIndexCount = 5; // number of screens aka 'states'
-let stateCharLength = 0; // number of characters in a given stateIndex as calculated by string()
-let stateDelay = 0; // a length of time or other calculation representing how long the state remains before changing)
+let counter = 0; // ticks come from the rotary encoder via Arduino over serial
+let sceneIndex = 0;
+let sceneCount = 5; // number of screens aka 'scenes'
+let sceneCharLength = 0; // number of characters in a given stateIndex as calculated by string()
+let sceneDelay = 0; // a length of time or other calculation representing how long the state remains before changing)
 
 
 function setup() {
@@ -11,26 +10,25 @@ function setup() {
 }
 
 function draw() {
-  pot = 0; // serial read the Arduino value
-  ticks += (pot - 512) / 512; // this math gives us a value between 0 and 1
+  counter += (pot - 512) / 512; // this math gives us a value between 0 and 1
 
-  if (ticks >= (stateCharLength + stateDelay)) {
-    nextState(); // function to move to the next screen aka 'state'
-    ticks = 0; // TODO: if I reset ticks on the p5 side, what do I do with ticks on the Arduino side?
-  } else if (ticks <= (stateCharLength + stateDelay)) {
-    priorState(); // function to move to the proceeding screen aka 'state
-    ticks = 0;
+  if (counter >= (sceneCharLength + sceneDelay)) {
+    showNextScene(); // function to move to the next screen aka 'state'
+    counter = 0; // TODO: if I reset ticks on the p5 side, what do I do with ticks on the Arduino side?
+  } else if (counter <= (sceneCharLength + sceneDelay)) {
+    showPreviousScene(); // function to move to the proceeding screen aka 'state
+    counter = 0;
   }
 }
 
 // functions to control which state to change to
 
-function nextState() {
-  // clear text on screen
-  ((stateIndexCount + stateCurrentIndex) + 0.5) % stateIndexCount
+function showNextScene() {
+  // clear screen
+  ((sceneCount + sceneIndex) + 0.5) % sceneCount
 }
 
-function priorState() {
-  // clear text on screen
-  ((stateIndexCount + stateCurrentIndex) - 0.5) % stateIndexCount
+function showPreviousScene() {
+  // clear screen
+  ((sceneCount + sceneIndex) - 0.5) % sceneCount
 }
