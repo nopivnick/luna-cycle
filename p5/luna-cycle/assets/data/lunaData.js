975cce00238e94d725137f2cb5f53b2805b04b91
nopivnick <nopivnick@nopivnick.com>
2021-02-01 11:38:08 -0500

    Add data files to main sketch

---
+++ p5/luna-cycle/assets/data/lunaData.js
@@ -0,0 +1,111 @@
+const lunaData = {
+  scene: [{
+      paragraph: [{
+          base: "They come while I write.",
+          style: ".default",
+          snippets: null
+        },
+        {
+          base: "Actias luna.",
+          style: ".default",
+          snippets: null
+        },
+        {
+          base: "${0}, big as your ${1}, they ${2} themselves on the window closest to the light and pitter-patter against the glass with lime-green wings of paper and dust.",
+          style: ".default",
+          snippets: [{
+              bliss: "Delicate and otherworldly",
+              blah: "Large and clumsy",
+              bad: "Grotesque and shabby"
+            },
+            {
+              bliss: "palm",
+              blah: "hand",
+              bad: "fist"
+            },
+            {
+              bliss: "flaunt",
+              blah: "park",
+              bad: "foist"
+
+            }
+          ]
+        },
+        {
+          base: "Do you remember?",
+          style: ".text-sent",
+          snippets: null
+        },
+        {
+          base: "That night we ${0} them, ${1} and ${2}, facing opposite directions with the ${3} of ${4}.",
+          style: ".default",
+          snippets: {
+            s0: {
+              bliss: "marveled at",
+              blah: "pondered",
+              bad: "pitied"
+            },
+            s1: {
+              bliss: "pristine",
+              blah: "frayed",
+              bad: "torn"
+            },
+            s2: {
+              bliss: "demure",
+              blah: "shabby",
+              bad: "battered"
+            },
+            s3: {
+              bliss: "awkward reticence",
+              blah: "familiar disinterest",
+              bad: "forlorn resentment"
+            },
+            s4: {
+              bliss: "new found lovers",
+              blah: "steadfast partners",
+              bad: "lifelong counterparts"
+            }
+          }
+        }
+      ]
+    },
+    {
+      paragraph: [{
+          base: "I've just closed the laptop lid when the screen on my phone grows bright.",
+          style: ".default",
+          snippets: null
+        },
+        {
+          base: "It's ${0} how you always seem to know.",
+          style: ".default",
+          snippets: {
+            s0: {
+              bliss: "thrilling",
+              blah: "eerie",
+              bad: "irksome"
+            }
+          }
+        }
+      ]
+    },
+    {
+      paragraph: [{
+          base: "We speak in text.",
+          style: ".default",
+          snippets: null
+        },
+        {
+          base: "And yet, you can't help but feel these words ${0} as you type them on the screen:",
+          style: ".default",
+          snippets: {
+            s0: {
+              bliss: "come effortlessly",
+              blah: "feel tired",
+              bad: "seem scripted"
+            }
+          }
+        }
+      ]
+    }
+  ]
+}
