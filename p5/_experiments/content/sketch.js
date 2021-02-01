abfcfb95b4d43b482da4e5185bebff0d3a09ccf2
nopivnick <nopivnick@nopivnick.com>
2021-02-01 11:47:06 -0500

    Update index.html & sketch.js

--- p5/_experiments/content/sketch.js
+++ p5/_experiments/content/sketch.js
@@ -1,7 +1,27 @@
+const bliss = 'bliss';
+const blah = 'blah';
+const bad = 'bad';
+
+const tones = ['bliss', 'blah', 'bad'];
+
+const sceneCount = 11;
+
 function setup() {
-  createCanvas(400, 400);
+  setupCanvas();
+  createDiv(setupScreen0HTML(bliss));
+  createDiv(setupScreen0HTML(blah));
+  createDiv(setupScreen0HTML(bad));
+  console.log(setupScreen0(bliss));
+  console.log(setupScreen0(blah));
+  console.log(setupScreen0(bad));
 }
 
 function draw() {
-  background(220);
+  background(0);
+}
+
+function setupCanvas() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
 }
