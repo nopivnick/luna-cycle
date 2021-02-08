0246b680f24830fbce58faeaea6721e386487fab
nopivnick <nopivnick@nopivnick.com>
2021-02-08 00:08:59 -0500

    Update lunaData.js

--- p5/luna-cycle/assets/data/lunaData.js
+++ p5/luna-cycle/assets/data/lunaData.js
@@ -2,17 +2,17 @@ const lunaData = {
   scene: [{
       paragraph: [{
           base: "They come while I write.",
-          style: ".default",
+          cssClass: null,
           snippets: null
         },
         {
           base: "Actias luna.",
-          style: ".default",
+          cssClass: null,
           snippets: null
         },
         {
-          base: "${0}, big as your ${1}, they ${2} themselves on the window closest to the light and pitter-patter against the glass with lime-green wings of paper and dust.",
-          style: ".default",
+          base: "{0}, big as your {1}, they {2} themselves on the window closest to the light and pitter-patter against the glass with lime-green wings of paper and dust.",
+          cssClass: null,
           snippets: [{
               bliss: "Delicate and otherworldly",
               blah: "Large and clumsy",
@@ -27,83 +27,77 @@ const lunaData = {
               bliss: "flaunt",
               blah: "park",
               bad: "foist"
-
             }
           ]
         },
         {
           base: "Do you remember?",
-          style: ".text-sent",
+          cssClass: "messages message text-sent",
           snippets: null
         },
         {
-          base: "That night we ${0} them, ${1} and ${2}, facing opposite directions with the ${3} of ${4}.",
-          style: ".default",
-          snippets: {
-            s0: {
+          base: "That night we {0} them, {1} and {2}, facing opposite directions with the {3} of {4}.",
+          cssClass: null,
+          snippets: [{
               bliss: "marveled at",
               blah: "pondered",
               bad: "pitied"
             },
-            s1: {
+            {
               bliss: "pristine",
               blah: "frayed",
               bad: "torn"
             },
-            s2: {
+            {
               bliss: "demure",
               blah: "shabby",
               bad: "battered"
             },
-            s3: {
+            {
               bliss: "awkward reticence",
               blah: "familiar disinterest",
               bad: "forlorn resentment"
             },
-            s4: {
+            {
               bliss: "new found lovers",
-              blah: "steadfast partners",
-              bad: "lifelong counterparts"
+              blah: "steadfast companions",
+              bad: "lifelong partners"
             }
-          }
+          ]
         }
       ]
     },
     {
       paragraph: [{
           base: "I've just closed the laptop lid when the screen on my phone grows bright.",
-          style: ".default",
+          cssClass: null,
           snippets: null
         },
         {
-          base: "It's ${0} how you always seem to know.",
-          style: ".default",
-          snippets: {
-            s0: {
-              bliss: "thrilling",
-              blah: "eerie",
-              bad: "irksome"
-            }
-          }
+          base: "It's {0} how you always seem to know.",
+          cssClass: null,
+          snippets: [{
+            bliss: "thrilling",
+            blah: "eerie",
+            bad: "irksome"
+          }]
         }
       ]
     },
     {
       paragraph: [{
           base: "We speak in text.",
-          style: ".default",
+          cssClass: null,
           snippets: null
         },
         {
-          base: "And yet, you can't help but feel these words ${0} as you type them on the screen:",
-          style: ".default",
-          snippets: {
-            s0: {
-              bliss: "come effortlessly",
-              blah: "feel tired",
-              bad: "seem scripted"
-            }
-          }
+          base: "And yet, you can't help but feel these words {0} as you type them on the screen:",
+          cssClass: null,
+          snippets: [{
+            bliss: "come effortlessly",
+            blah: "feel tired",
+            bad: "seem scripted"
+          }]
         }
       ]
     }
