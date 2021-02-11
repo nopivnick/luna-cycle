40aeebf05bff9c9d1912f129357a08c1f886e833
nopivnick <nopivnick@nopivnick.com>
2021-02-11 11:27:59 -0500

    Update sketch.js

--- p5/_experiments/content-replace/sketch.js
+++ p5/_experiments/content-replace/sketch.js
@@ -19,27 +19,28 @@ function setupCanvas() {
 }
 
 function setupScreen() { // TODO: add parameters for SCENE and TONE (?)
-  for (i = 0; i < lunaData.scene.length; i++) { // for every scene in lunaData
+  for (i = 0; i < lunaData.scene.length; i++) { // for every scene in the lunaData object
     // console.log("Scene " + i);
-    for (j = 0; j < lunaData.scene[i].paragraph.length; j++) { // for every paragraph of every scene in lunaData
+    for (j = 0; j < lunaData.scene[i].paragraph.length; j++) { // for every paragraph of every scene in the lunaData object
       // console.log("Scene " + i + " Paragraph " + j);
       if (lunaData.scene[i].paragraph[j].snippets === null) { // if key 'snippets' has value null
         // console.log("NO Snippets!");
         let paragraph = lunaData.scene[i].paragraph[j].base;
         console.log(paragraph);
-        let newParagraph = createP(paragraph); // wrap base sentence in <p></p> tags and ...
-        if (lunaData.scene[i].paragraph[j].cssClass) { // ... if the paragraph's key cssClass has a value other than null ...
-          newParagraph.addClass(lunaData.scene[i].paragraph[j].cssClass); // ... apply the assigned CSS class.
+        paragraph = createP(paragraph); // wrap base sentence in <p></p> tags and ...
+        if (lunaData.scene[i].paragraph[j].cssClass !== null) { // ... if the paragraph's key cssClass has a value other than null ...
+          paragraph.addClass(lunaData.scene[i].paragraph[j].cssClass); // ... apply the assigned CSS class.
         }
       } else {
         // console.log("YES Snippets!");
-        console.log(lunaData.scene[i].paragraph[j].base.replace(regex, function getSnippet() {
+        paragraph = lunaData.scene[i].paragraph[j].base.replace(regex, function getSnippet() {
           for (k = 0; k < lunaData.scene[i].paragraph[j].snippets.length; k++) {
             return lunaData.scene[i].paragraph[j].snippets[k][tone];
           }
-        }));
+        });
+        console.log(paragraph);
+        paragraph = createP(paragraph);
 
-        createP(lunaData.scene[i].paragraph[j].base.replace(regex, '*SNIPPET*'));
         if (lunaData.scene[i].paragraph[j].cssClass) {
           paragraph.addClass(lunaData.scene[i].paragraph[j].cssClass);
         }
@@ -48,10 +49,6 @@ function setupScreen() { // TODO: add parameters for SCENE and TONE (?)
   }
 }
 
-function getSnippet(match) {
-  // TODO: return the value of the snippet with key = tone;
-}
-
-function setupScene(screen) {
-
-}
+// function getSnippet(match) {
+//   // TODO: return the value of the snippet with key === variable tone;
+// }
