872064483d14346a91c539d5bc4d01a254eac47b
nopivnick <nopivnick@nopivnick.com>
2021-02-11 09:31:43 -0500

    Update sketch.js

--- p5/_experiments/content-replace/sketch.js
+++ p5/_experiments/content-replace/sketch.js
@@ -1,7 +1,7 @@
 let screens = [];
 let tone = 'bliss';
 
-const regex = /{\d}/g;
+const regex = /\{\d\}/g;
 
 function setup() {
   setupCanvas();
@@ -18,26 +18,29 @@ function setupCanvas() {
   canvas.style('z-index', '-1');
 }
 
-function setupScreen() { // TODO: add parameters for SCENE and TONE
-  for (i = 0; i < lunaData.scene.length; i++) {
+function setupScreen() { // TODO: add parameters for SCENE and TONE (?)
+  for (i = 0; i < lunaData.scene.length; i++) { // for every scene in lunaData
     // console.log("Scene " + i);
-    for (j = 0; j < lunaData.scene[i].paragraph.length; j++) {
+    for (j = 0; j < lunaData.scene[i].paragraph.length; j++) { // for every paragraph of every scene in lunaData
       // console.log("Scene " + i + " Paragraph " + j);
-      if (lunaData.scene[i].paragraph[j].snippets === null) {
+      if (lunaData.scene[i].paragraph[j].snippets === null) { // if key 'snippets' has value null
         // console.log("NO Snippets!");
         let paragraph = lunaData.scene[i].paragraph[j].base;
         console.log(paragraph);
-        createP(paragraph);
-        if (lunaData.scene[i].paragraph[j].cssClass) {
-          // console.log(lunaData.scene[i].paragraph[j].cssClass);
-          paragraph.addClass(lunaData.scene[i].paragraph[j].cssClass);
+        let newParagraph = createP(paragraph); // wrap base sentence in <p></p> tags and ...
+        if (lunaData.scene[i].paragraph[j].cssClass) { // ... if the paragraph's key cssClass has a value other than null ...
+          newParagraph.addClass(lunaData.scene[i].paragraph[j].cssClass); // ... apply the assigned CSS class.
         }
       } else {
         // console.log("YES Snippets!");
-        console.log(lunaData.scene[i].paragraph[j].base.replaceAll(regex, '*SNIPPET*'));
-        createP(lunaData.scene[i].paragraph[j].base.replaceAll(regex, '*SNIPPET*'));
+        console.log(lunaData.scene[i].paragraph[j].base.replace(regex, function getSnippet() {
+          for (k = 0; k < lunaData.scene[i].paragraph[j].snippets.length; k++) {
+            return lunaData.scene[i].paragraph[j].snippets[k][tone];
+          }
+        }));
+
+        createP(lunaData.scene[i].paragraph[j].base.replace(regex, '*SNIPPET*'));
         if (lunaData.scene[i].paragraph[j].cssClass) {
-          // console.log(lunaData.scene[i].paragraph[j].cssClass);
           paragraph.addClass(lunaData.scene[i].paragraph[j].cssClass);
         }
       }
