13bb36d44a1705d0b1dd13f3a936598d4e03347f
nopivnick <nopivnick@nopivnick.com>
2021-02-24 20:07:28 -0500

    Update screens.js
    - Add initial attempt at a `getBase()` function
    - Add logic to handle `<spans>` in screens w/ text message vs. narrative
    - Refactor initial alphaValue logic into as stand alone function `setAlpha()`

--- p5/luna-cycle/scripts/screens.js
+++ p5/luna-cycle/scripts/screens.js
@@ -5,6 +5,7 @@ let green;
 let blue;
 
 function setupScreen(scene, tone) {
+  // getBase(); // TODO: should this be it's own function?
   for (i = 0; i < lunaData.scenes[scene].paragraphs.length; i++) { // for every paragraph of the current scene ...
     if (lunaData.scenes[scene].paragraphs[i].snippets === null) { // if there are no tone variants ...
       base = lunaData.scenes[scene].paragraphs[i].base; // get the base sentence ...
@@ -20,16 +21,15 @@ function setupScreen(scene, tone) {
   // resetCursor();
 }
 
-// function getBase() { // TODO: this function is not working
-//   // let base;
+// function getBase() { // TODO: perhaps this function should have the `scene` & `tone` parameters?
 //   for (i = 0; i < lunaData.scenes[scene].paragraphs.length; i++) { // for every paragraph of the current scene ...
-//     if (lunaData.scenes[scene].paragraphs[i].snippets !== null) { // if the base sentence has variants based on tone ...
+//     if (lunaData.scenes[scene].paragraphs[i].snippets === null) { // if there are no tone variants ...
 //       base = lunaData.scenes[scene].paragraphs[i].base; // get the base sentence ...
+//     } else { // otherwise ...
+//       base = lunaData.scenes[scene].paragraphs[i].base;
 //       for (j = 0; j < lunaData.scenes[scene].paragraphs[i].snippets.length; j++) {
 //         base = base.replace('{' + j + '}', lunaData.scenes[scene].paragraphs[i].snippets[j][tone]);
 //       }
-//     } else {
-//       base = lunaData.scenes[scene].paragraphs[i].base;
 //     }
 //     console.log(base);
 //   }
@@ -37,19 +37,21 @@ function setupScreen(scene, tone) {
 
 function setupCharacters(base) {
   let paragraph = createP();
-  for (k = 0; k < base.length; k++) {
-    let character = base.charAt(k);
-    let span = createSpan(character);
-    span.style('position: relative');
-    setColor();
-    if (isAlphaOn === true) {
-      span.style('color: rgba('+red+', '+green+', '+blue+', 0)'); // TODO: better to handle alpha with a class in css? 
-    } else {
-      span.style('color: rgba('+red+', '+green+', '+blue+', 1)');
+  setColor();
+  if (lunaData.scenes[scene].paragraphs[i].cssClass === null) {
+    for (k = 0; k < base.length; k++) {
+      let character = base.charAt(k);
+      let span = createSpan(character);
+      span.style('position: relative');
+      setAlpha(span);
+      paragraph.child(span); // make each <span>char</span> a child of the paragraph
     }
-    paragraph.child(span); // make each <span>char</span> a child of the paragraph
+    return paragraph;
+  } else {
+    let span = createSpan(base)
+    paragraph.child(span);
+    return paragraph;
   }
-  return paragraph;
 }
 
 function setCSS(paragraph) {
@@ -67,6 +69,14 @@ function setColor() {
     red = 50;
     green = 205;
     blue = 50;
+  }
+}
+
+function setAlpha(span) {
+  if (isAlphaOn === true) {
+    span.style('color: rgba(' + red + ', ' + green + ', ' + blue + ', 0)'); // TODO: possible to handle alpha with a class in css? 
+  } else {
+    span.style('color: rgba(' + red + ', ' + green + ', ' + blue + ', 1)');
   }
 }
 
