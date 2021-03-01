3b897a17d22441e1472e50d1641f15a7f270c78a
nopivnick <nopivnick@nopivnick.com>
2021-03-01 11:13:41 -0500

    Refactor code
    - Move `toggleAlpha()`, `toggleCursor()`, & `toggleMirror()` functions → `hotkeys.js`

--- p5/luna-cycle/public/scripts/hotkeys.js
+++ p5/luna-cycle/public/scripts/hotkeys.js
@@ -15,3 +15,53 @@ function playtestKeyPressed() {
   }
   print(key + " pressed!");
 }
+
+function toggleAlpha() {
+  if (isAlphaOn === false) {
+    screen = select('body');
+    screen.style(`color: rgba(${red}, ${green}, ${blue}, 1)`);
+    isAlphaOn = !isAlphaOn;
+  } else {
+    screen = select('body');
+    screen.style(`color: rgba(${red}, ${green}, ${blue}, 0)`);
+    isAlphaOn = !isAlphaOn;
+  }
+}
+
+function toggleCursor() {
+  if (isCursorDisplayed === false) {
+    screen = select('body');
+    screen.style('cursor: none')
+    isCursorDisplayed = !isCursorDisplayed;
+  } else {
+    screen = select('body');
+    screen.style('cursor: auto')
+    isCursorDisplayed = !isCursorDisplayed;
+  }
+}
+
+function toggleMirror() {
+  screen = select('body'); // TODO: cleaner to either mirror a div wrapped around the multiple <p>'s or selectAll <p>'s?
+  // screen = select('div');
+  if (isScreenMirrored === true) {
+    screen.style('transform: none');
+    // canvas.style('transform: none');
+    isScreenMirrored = !isScreenMirrored;
+  } else {
+    screen.style('transform: rotateY(180deg)');
+    // canvas.style('transform: rotateY(180deg)');
+    isScreenMirrored = !isScreenMirrored;
+  }
+//   paragraphs = selectAll('p');
+//   if (isScreenMirrored === true) {
+//     for (i = 0; i < paragraphs.length; i++) {
+//       this.style('transform: none');
+//     }
+//     isScreenMirrored = !isScreenMirrored;
+//   } else {
+//     for (i = 0; i < paragraphs.length; i++) {
+//       this.style('transform: rotateY(180deg)');
+//     }
+//     isScreenMirrored = !isScreenMirrored;
+//   }
+}
--- p5/luna-cycle/public/sketch.js
+++ p5/luna-cycle/public/sketch.js
@@ -1,3 +1,5 @@
+var socket = io();
+
 let canvas;
 
 let screen;
@@ -111,57 +113,6 @@ function drawScene() {
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   console.log("window size: " + windowWidth + " x " + windowHeight);
-}
-
-
-function toggleAlpha() {
-  if (isAlphaOn === false) {
-    screen = select('body');
-    screen.style(`color: rgba(${red}, ${green}, ${blue}, 1)`);
-    isAlphaOn = !isAlphaOn;
-  } else {
-    screen = select('body');
-    screen.style(`color: rgba(${red}, ${green}, ${blue}, 0)`);
-    isAlphaOn = !isAlphaOn;
-  }
-}
-
-function toggleCursor() {
-  if (isCursorDisplayed === false) {
-    screen = select('body');
-    screen.style('cursor: none')
-    isCursorDisplayed = !isCursorDisplayed;
-  } else {
-    screen = select('body');
-    screen.style('cursor: auto')
-    isCursorDisplayed = !isCursorDisplayed;
-  }
-}
-
-function toggleMirror() {
-  screen = select('body'); // TODO: cleaner to mirror a div wrapped around the multiple <p>'s?
-  // screen = select('div');
-  if (isScreenMirrored === true) {
-    screen.style('transform: none');
-    // canvas.style('transform: none');
-    isScreenMirrored = !isScreenMirrored;
-  } else {
-    screen.style('transform: rotateY(180deg)');
-    // canvas.style('transform: rotateY(180deg)');
-    isScreenMirrored = !isScreenMirrored;
-  }
-  // paragraphs = selectAll('p');
-  // if (isScreenMirrored === true) {
-  //   for (i = 0; i < paragraphs.length; i++) {
-  //     this.style('transform: none');
-  //   }
-  //   isScreenMirrored = !isScreenMirrored;
-  // } else {
-  //   for (i = 0; i < paragraphs.length; i++) {
-  //     this.style('transform: rotateY(180deg)');
-  //   }
-  //   isScreenMirrored = !isScreenMirrored;
-  // }
 }
 
 function getSceneNum() {
