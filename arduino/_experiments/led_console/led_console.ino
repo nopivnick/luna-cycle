8c69788424840e11dfffdf141f7962c70018939b
nopivnick <nopivnick@nopivnick.com>
2021-03-10 13:26:21 -0500

    Fix typo in file name

--- arduino/_experiments/led_console/led_console.ino
+++ arduino/_experiments/led_console/led_console.ino
@@ -1,3 +1,9 @@
+const int ledRingUserA = 10;
+const int ledRingUserB = 11;
+
+const int ledStripUserA = 12;
+const int ledStripUserB = 13;
+
 void setup() {
   // put your setup code here, to run once:
 
