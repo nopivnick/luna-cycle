0a876d3e420dbcbe1d35fd52a0325a9b8df49664
nopivnick <nopivnick@nopivnick.com>
2021-03-10 13:27:20 -0500

    Create pot.ino

---
+++ arduino/_experiments/pot/pot.ino
@@ -0,0 +1,12 @@
+const int potUserA = A0;
+const int potUserB = A1;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
