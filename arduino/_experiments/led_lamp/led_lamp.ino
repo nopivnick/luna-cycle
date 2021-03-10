057bcd4c212a99002010a28645a8eebc63e0e989
nopivnick <nopivnick@nopivnick.com>
2021-03-10 13:27:05 -0500

    Update `led_lamp.ino`

--- arduino/_experiments/led_lamp/led_lamp.ino
+++ arduino/_experiments/led_lamp/led_lamp.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+const int lampUserA = 8;
+const int lampUserB = 9;
 
+void setup() {
+  Serial.begin(115200);
+  pinMode(lampUserA, OUTPUT);
+  pinMode(lampUserB, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (Serial.available() > 0) { // If there's data in the serial buffer ...
+    char input = Serial.read(); // read it and assign it to a variable.
+    if (input == 'A') {
+      digitalWrite(lampUserA, HIGH);
+    }
+    if (input == 'a') {
+      digitalWrite(lampUserA, LOW);
+    }
+    if (input == 'B') {
+      digitalWrite(lampUserB, HIGH);
+    }
+    if (input == 'b') {
+      digitalWrite(lampUserB, LOW);
+    }
+  }
 }
