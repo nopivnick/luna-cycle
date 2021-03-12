16dee52e6cd4659efc0563356dd7787821983e88
nopivnick <nopivnick@nopivnick.com>
2021-03-12 13:13:31 -0500

    Create test_messages.ino

---
+++ arduino/_experiments/test_messages/test_messages.ino
@@ -0,0 +1,14 @@
+int pauseDuration = 100;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("start");
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("tick++");
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(pauseDuration);
+}
