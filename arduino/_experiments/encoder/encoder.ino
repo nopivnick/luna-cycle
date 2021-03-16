59f707e9252f3911b4e3313beb45bf11dca5454f
nopivnick <nopivnick@nopivnick.com>
2021-03-15 21:44:12 -0400

    Update encoder.ino

--- arduino/_experiments/encoder/encoder.ino
+++ arduino/_experiments/encoder/encoder.ino
@@ -6,28 +6,60 @@
 
 Encoder ENCODER(2, 3); // Interrupt pins on the UNO are 2 & 3
 
-//const int ENCODER_BUTTON = 4;
-//int encoderButtonState = 0;
-
 void setup() {
   Serial.begin(115200);
-//  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  Serial.println("Basic Encoder Test:");
 }
 
+const long encoderPulseInterval = 1000;
+unsigned long prevEncoderPulse = 0;
+
+long encoder = -999;
 long previousEncoder = -999;
 
+unsigned long timeStamp = 0;
+unsigned long previousTimeStamp = 0;
+
+bool isSpinning = false;
+bool isGoTime = false;
+
 void loop() {
-  long encoder = ENCODER.read();
+  timeStamp = millis();
+  updateEncoder();
+  updateState();
+}
+
+void updateEncoder() {
+  encoder = ENCODER.read();
   long tempEncoder = (encoder / 4);
   if (tempEncoder != previousEncoder) {
-    previousEncoder = tempEncoder;
     Serial.println(tempEncoder);
+    prevEncoderPulse = timeStamp;
+    previousEncoder = tempEncoder;
   }
+  if (timeStamp - prevEncoderPulse <= encoderPulseInterval) {
+    isSpinning = true;
+    //    Serial.println("SPINNING!");
+  } else {
+    isSpinning = false;
+    //    Serial.println("NOT SPINNING!");
+  }
+  resetEncoder();
+}
+
+void resetEncoder() {
   if (Serial.available() > 0) {
     char input = Serial.read();
     if (input == 'r') {
       ENCODER.write(0);
     }
+  }
+}
+
+void updateState() {
+  isGoTime = isSpinning;
+  if (isGoTime == true) {
+    Serial.println("GO!");
+  } else if (!isGoTime) {
+    Serial.println("NO!");
   }
 }
