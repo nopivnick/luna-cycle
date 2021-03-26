3d24ab1fb9222a4895ce7fa81779c3f89aaa04e2
nopivnick <nopivnick@nopivnick.com>
2021-03-26 19:03:06 -0400

    Move encoder code from `encoder.io` → `luna-cycle.ino`

--- arduino/luna-cycle/luna-cycle.ino
+++ arduino/luna-cycle/luna-cycle.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include "Adafruit_MPR121.h"
+#include <Encoder.h>
 
 #ifndef _BV
 #define _BV(bit) (1 << (bit))
@@ -16,12 +17,29 @@ const long capPlateInterval = 1000;
 unsigned long prevCapTouchPlateUserA = 0;
 unsigned long prevCapTouchPlateUserB = 0;
 
+// Best Performance: both pins have interrupt capability
+// Good Performance: only the first pin has interrupt capability
+// Low Performance:  neither pin has interrupt capability
+
+Encoder ENCODER(2, 3); // Interrupt pins on the UNO are 2 & 3
+
+const long encoderPulseInterval = 1000;
+unsigned long prevEncoderPulse = 0;
+
+long encoder = -999;
+long previousEncoder = -999;
+
 unsigned long timeStamp = 0;
 unsigned long previousTimeStamp = 0;
 
 bool isUserA_touchingPlate = false;
 bool isUserB_touchingPlate = false;
 bool isAandB_touchingPlates = false;
+
+bool isSpinning = false;
+bool isSpinningFwd = false;
+bool isSpinningBkwd = false;
+
 bool isGoTime = false;
 
 void setup() {
@@ -31,9 +49,7 @@ void setup() {
     delay(10);
   }
 
-  Serial.println("Adafruit MPR121 Capacitive Touch sensor test");
-
-  // Default address is 0x5A, if tied to 3.3V its 0x5B
+  // Default MPR121 address is 0x5A, if tied to 3.3V its 0x5B
   // If tied to SDA its 0x5C and if SCL then 0x5D
   if (!MPR121.begin(0x5A)) {
     Serial.println("MPR121 not found, check wiring?");
@@ -46,6 +62,7 @@ void loop() {
   timeStamp = millis();
   updateMPR121();
   updatePlates();
+  updateEncoder();
   updateState();
 }
 
@@ -71,12 +88,12 @@ void updatePlates() {
   }
   if (timeStamp - prevCapTouchPlateUserA <= capPlateInterval) { // ... and it's been less than a certain interval ...
     isUserA_touchingPlate = true; // ... user A is considered to be touching their plate.
-//    Serial.print("isUserA_touchingPlate is ");
-//    Serial.println(isUserA_touchingPlate);
+    //    Serial.print("isUserA_touchingPlate is ");
+    //    Serial.println(isUserA_touchingPlate);
   } else {
     isUserA_touchingPlate = false;
-//    Serial.print("isUserA_touchingPlate is ");
-//    Serial.println(isUserA_touchingPlate);
+    //    Serial.print("isUserA_touchingPlate is ");
+    //    Serial.println(isUserA_touchingPlate);
   }
   if (MPR121.touched() & (1 << 1)) { // If capacitive sensor 1 is touched ...
     prevCapTouchPlateUserB = timeStamp;
@@ -99,8 +116,50 @@ void updatePlates() {
   }
 }
 
+void updateEncoder() {
+  encoder = ENCODER.read();
+  long tempEncoder = (encoder / 4); // TODO: should this just be encoder = (encoder / 4)?
+  //  Serial.print(tempEncoder);
+  //  Serial.print(" ");
+  //  Serial.println(previousEncoder);
+  if (tempEncoder != previousEncoder) {
+    Serial.println(tempEncoder);
+    prevEncoderPulse = timeStamp;
+    if (tempEncoder > previousEncoder) {
+      bool isSpinningFwd = true;
+      bool isSpinningBkwd = false;
+    } else if (tempEncoder < previousEncoder) {
+      bool isSpinningFwd = false;
+      bool isSpinningBkwd = true;
+    }
+    previousEncoder = tempEncoder;
+  }
+  if (timeStamp - prevEncoderPulse <= encoderPulseInterval) { // TODO: this is true at boot!
+    isSpinning = true; // TODO: how not to flip this boolean until after 1 interval from boot?
+    //    Serial.println("SPINNING!");
+  } else {
+    isSpinning = false;
+    bool isSpinningFwd = false;
+    bool isSpinningBkwd = false;
+    //    Serial.println("NOT SPINNING!");
+  }
+  resetEncoder();
+}
+
+void resetEncoder() {
+  if (Serial.available() > 0) {
+    char input = Serial.read();
+    if (input == 'r') {
+      ENCODER.write(0);
+    }
+  }
+}
+
 void updateState() {
-  isGoTime = isAandB_touchingPlates;
+  //  Serial.print(isSpinning);
+  //  Serial.print(" ");
+  //  Serial.println(isGoTime);
+  isGoTime = isAandB_touchingPlates && isSpinning;
   if (isGoTime) {
     Serial.println("GO!");
   } else if (!isGoTime) {
