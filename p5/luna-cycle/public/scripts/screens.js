17765eaf80b03386e62874d9d115133bb49bea7c
nopivnick <nopivnick@nopivnick.com>
2021-04-06 14:58:36 -0400

    Refactor code
    - Rename function `setupCharacters()` → `setupParagraphs()`

--- p5/luna-cycle/public/scripts/screens.js
+++ p5/luna-cycle/public/scripts/screens.js
@@ -8,11 +8,10 @@ function setupScreen(scene, tone) {
   container = createDiv();
   container.parent('mirror');
   base = getBase(scene, tone);
-  // setCSS(setupCharacters(base, i), i); // TODO: fix error when moving this here
+  // setCSS(setupParagraphs(base, i), i); // TODO: fix error when moving this here
 }
 
 function getBase(scene, tone) {
-
     for (i = 0; i < lunaData.scenes[scene].paragraphs.length; i++) { // for every paragraph of the current scene ...
       if (lunaData.scenes[scene].paragraphs[i].snippets === null) { // if there are no tone variants ...
         base = lunaData.scenes[scene].paragraphs[i].base; // get the base sentence ...
@@ -23,18 +22,15 @@ function getBase(scene, tone) {
         }
       }
       console.log(base);
-      setCSS(setupCharacters(base, i), i); // TODO: move this to setupScreen()
+      setCSS(setupParagraphs(base, i), i); // TODO: move this to setupScreen()
     }
   return base;
-
 }
 
-function setupCharacters(base, i) {
+function setupParagraphs(base, i) {
   let paragraph = createP();
   container.child(paragraph);
   setColor(i);
-
-  if (lunaData.scenes[scene].paragraphs[i].cssClass === null) {
     for (k = 0; k < base.length; k++) {
       let character = base.charAt(k);
       let span = createSpan(character);
@@ -43,13 +39,6 @@ function setupCharacters(base, i) {
       paragraph.child(span); // make each <span>char</span> a child of the paragraph
     }
     return paragraph;
-
-  } else {
-    let span = createSpan(base)
-    paragraph.child(span);
-    return paragraph;
-  }
-
 }
 
 console.log("scenes.js LOADED");
